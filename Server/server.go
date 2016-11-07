0b26ed7058e29a493895ce15f1f8a3019a5ea841
Tomas Barry <tomas.barry@hotmail.com>
2016-11-07 19:32:57 +0000

    Added some error handling and documentation

--- Server/server.go
+++ Server/server.go
@@ -18,19 +18,31 @@ var (
 
 func handleConnection(conn net.Conn) {
 	fmt.Println("Handling connection ", activeThreads)
-	buff, _ := ioutil.ReadAll(conn)
-	fmt.Println(string(buff))
+	buff, e := ioutil.ReadAll(conn)
+	handleError(e)
+	message := string(buff)
 	activeThreads -= 1
 	fmt.Println("Handled connection ", activeThreads)
+}
+
+func handleError(e error) {
+	// if an error exits, exit the program
+	if e {
+		fmt.Println("An error occurred: %s", e.Error())
+		os.Exit(1)
+	}
 }
 
 func main() {
 	// accept command line arguements where args[0] is the port number to run on
 	args := os.Args[1:]
-	listener, _ := net.Listen(CONN_TYPE, ":" + args[0])
+	// create the listener
+	listener, e := net.Listen(CONN_TYPE, ":" + args[0])
+	handleError(e)
 	// wait for new clients to connect
 	for {
-		conn, _ := listener.Accept()
+		conn, e := listener.Accept()
+		handleError(e)
 		if activeThreads < MAX_THREAD_POOL {
 			fmt.Println("Less than max number of threads")
 			activeThreads += 1
