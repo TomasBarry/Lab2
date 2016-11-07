b5d58c2b498114237b8c40aea40d5e6d68c59c43
Tomas Barry <tomas.barry@hotmail.com>
2016-11-07 18:51:04 +0000

    Refresh of .gitignore

--- Server/server.go
+++ Server/server.go
@@ -7,31 +7,36 @@ import (
 )
 
 const (
-    MaxNumberOfThreads = 3
+	CONN_TYPE = "tcp"
+	MAX_THREAD_POOL = 1
 )
 
 var (
-    activeThreads = 0
+	activeThreads = 0
 )
 
 func handleConnection(conn net.Conn) {
-    fmt.Println("Handling connection ", activeThreads)
-    buff, _ := ioutil.ReadAll(conn)
-    fmt.Println(string(buff))
-    activeThreads -= 1
-    fmt.Println("Handled connection ", activeThreads)
+	fmt.Println("Handling connection ", activeThreads)
+	buff, _ := ioutil.ReadAll(conn)
+	fmt.Println(string(buff))
+	activeThreads -= 1
+	fmt.Println("Handled connection ", activeThreads)
 }
 
 func main() {
-    ln, _ := net.Listen("tcp", ":8000")
-    for {
-        conn, _ := ln.Accept()
-        if activeThreads < MaxNumberOfThreads {
-            fmt.Println("Less than max number of threads")
-            activeThreads += 1 
-            go handleConnection(conn)
-        } else {
-            fmt.Println("Too many threads already")
-        }
-    }
+	// accept command line arguements where args[0] is the port number to run on
+	args := os.Args[1:]
+	listener, _ := net.Listen(CONN_TYPE, ":" + args[0])
+	// wait for new clients to connect
+	for {
+		conn, _ := listener.Accept()
+		if activeThreads < MaxNumberOfThreads {
+			fmt.Println("Less than max number of threads")
+			activeThreads += 1
+			go handleConnection(conn)
+		} else {
+			fmt.Println("Too many threads already")
+		}
+	}
 }
+
