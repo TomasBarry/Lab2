7ab5a7043eea40ca71f0082e365345db822ae535
Tomas Barry <tomas.barry@hotmail.com>
2016-11-08 10:56:46 +0000

    Basic server completed

--- Server/server.go
+++ Server/server.go
@@ -5,11 +5,14 @@ import (
 	"io/ioutil"
 	"net"
 	"os"
+	"bytes"
 )
 
 const (
 	CONN_TYPE = "tcp"
 	MAX_THREAD_POOL = 1
+	KILL_COMMAND = "KILL_SERVICE\n"
+	HELLO_COMMAND ="HELO text\n"
 )
 
 var (
@@ -21,13 +24,30 @@ func handleConnection(conn net.Conn) {
 	buff, e := ioutil.ReadAll(conn)
 	handleError(e)
 	message := string(buff)
+	switch message {
+	case KILL_COMMAND:
+		os.Exit(0)
+	case HELLO_COMMAND:
+		// "HELO text\nIP:[ip address]\nPort:[port number]\nStudentID:[your student ID]\n"
+		var response bytes.Buffer
+		response.WriteString(message)
+		response.WriteString("IP:10.62.0.117\n")
+		response.WriteString("Port:8000\n")
+		response.WriteString("StudentID:13321218\n")
+		conn.Write([]byte(response.String()))
+		fmt.Println(response.String())
+	default:
+		// This would handle all other messages
+		fmt.Println(message)
+	}
 	activeThreads -= 1
 	fmt.Println("Handled connection ", activeThreads)
 }
 
+
 func handleError(e error) {
 	// if an error exits, exit the program
-	if e {
+	if e != nil {
 		fmt.Println("An error occurred: %s", e.Error())
 		os.Exit(1)
 	}
