b11b9542b1c94998295ccf2abff5734d56d3632f
Tomas Barry <tomas.barry@hotmail.com>
2016-11-08 13:42:56 +0000

    Abstracted some functionality

--- Server/server.go
+++ Server/server.go
@@ -11,45 +11,54 @@ import (
 const (
 	CONN_TYPE = "tcp"
 	MAX_THREAD_POOL = 10
+	IP_ADDR = "10.62.0.117"
+	STUDENT_NUMBER = "13321218"
 )
 
 var (
 	activeThreads = 0
 )
 
+func handleKillCommand(conn net.Conn) {
+	conn.Close()
+	activeThreads -= 1
+}
+
+func handleHeloCommand(message string, port string, conn net.Conn) {
+	// "HELO text\nIP:[ip address]\nPort:[port number]\nStudentID:[your student ID]\n"
+	var response bytes.Buffer
+	response.WriteString(message)
+	response.WriteString(fmt.Sprintf("IP:%s\n", IP_ADDR))
+	response.WriteString(fmt.Sprintf("Port:%s\n", port))
+	response.WriteString(fmt.Sprint("StudentID:%s\n", STUDENT_NUMBER))
+	conn.Write([]byte(response.String()))
+}
+
+func handleOtherCommand() {
+	// Stub function
+}
+
 func handleConnection(conn net.Conn) {
-	for {
-		// fmt.Println("Addr: ", conn.LocalAddr())
+	validHello := regexp.MustCompile(`HELO *`)
+	validKill := regexp.MustCompile(`KILL_SERVICE *`)
+	port := conn.LocalAddr().String()[12:]
+	// wait for messages on the socket
+	going := true
+	for going {
+		fmt.Println("Addr: ", conn.LocalAddr())
 		buff := make([]byte, 1024)
 		readLen, e := conn.Read(buff)
-		//fmt.Println("Message is : ", string(buff))
 		handleError(e)
 		message := string(buff[:readLen])
-		//fmt.Println(message)
-		validHello := regexp.MustCompile(`HELO *`)
-		validKill := regexp.MustCompile(`KILL_SERVICE *`)
 		if validKill.MatchString(message) {
-			fmt.Println("Gonna kill the server")
-			conn.Close()
-			os.Exit(0)
+			going = false
+			handleKillCommand(conn)
 		} else if  validHello.MatchString(message) {
-			fmt.Println("Got a helo message")
-			// "HELO text\nIP:[ip address]\nPort:[port number]\nStudentID:[your student ID]\n"
-			var response bytes.Buffer
-			response.WriteString(message)
-			response.WriteString("IP:10.62.0.117\n")
-			response.WriteString("Port:8000\n")
-			response.WriteString("StudentID:13321218\n")
-			conn.Write([]byte(response.String()))
-			handleError(e)
-			//fmt.Println(response.String())
+			handleHeloCommand(message, port, conn)
 		} else {
-			fmt.Println("Got a random message")
-			// This would handle all other messages
-			//fmt.Println(message)
+			handleOtherCommand()
 		}
 	}
-	//fmt.Println("Handled connection ", activeThreads)
 }
 
 
@@ -64,7 +73,6 @@ func handleError(e error) {
 func main() {
 	// accept command line arguements where args[0] is the port number to run on
 	args := os.Args[1:]
-	// create the listener
 	listener, e := net.Listen(CONN_TYPE, ":" + args[0])
 	handleError(e)
 	// wait for new clients to connect
@@ -73,11 +81,8 @@ func main() {
 		fmt.Println("A new connection")
 		handleError(e)
 		if activeThreads < MAX_THREAD_POOL {
-			//fmt.Println("Less than max number of threads")
 			activeThreads += 1
 			go handleConnection(conn)
-		} else {
-			//fmt.Println("Too many threads already")
 		}
 	}
 }
