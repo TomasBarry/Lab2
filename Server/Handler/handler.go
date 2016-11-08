4cdf74662c9699b3b134413f8bc3e5b57e2c735b
Tomas Barry <tomas.barry@hotmail.com>
2016-11-08 16:05:11 +0000

    Putting components into packages

---
+++ Server/Handler/handler.go
@@ -0,0 +1,23 @@
+package handler
+
+
+import (
+	"fmt"
+	"strings"
+)
+
+func HandleKill(conn Conn) {
+	conn.Close()
+}
+
+func HandleHelo(message string, conn Conn) {
+	// conn.LocalAddr() returns "10.62.0.117:8000" for example
+	// serverInfo[0] = 10.62.0.117
+	// serverInfo[1] = 8000
+	serverInfo := strings.Split(conn.localAddr(), ":")
+	conn.Write([]byte(fmt.Sprintf("%sIP:%s\nPort:%s\nStudentID:%s\n", message, serverInfo[0], serverInfo[1], "13321218"))
+}
+
+func HandleOther() {
+	// stub methond
+}
