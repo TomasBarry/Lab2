0937bfa3a11bbde4ddbd5b495880d639ae46a701
Tomas Barry <tomas.barry@hotmail.com>
2016-11-08 16:05:26 +0000

    Abstracted server code

---
+++ Server/HttpServer/httpserver.go
@@ -0,0 +1,45 @@
+package httpserver
+
+import (
+	"net"
+	"os"
+	"github.com/TomasBarry/Lab2/Handler"
+)
+
+const (
+	CONN_TYPE = "tcp"
+	HELO_COMMAND = "HELO "
+	KILL_COMMAND = "KILL_SERVICE\n"
+)
+
+func createServer() {
+	// accept command line arguements where args[0] is the port number to run on
+	args := os.args[1:]
+	listener, _ := net.Listen(CONN_TYPE, ":" + args[0])
+	// wait for connections
+	for {
+		conn, _ listener.Accept()
+		go handleConnection(conn)
+	}
+}
+
+func readSocket(conn Conn) message string {
+	buffer := make([]byte, 1024)
+	readLength, _ := conn.Read(buff)
+	message := string(buff[:readLength])
+}
+
+func handleConnection(conn Conn) {
+	// persist the socket connection
+	for {
+		switch message := readSocket(conn) {
+		case message.HasPrefix(HELO_COMMAND):
+			handler.HandleHelo(conn)
+		case message == KILL_COMMAND:
+			handler.HandleKill(conn)
+		default:
+			handler.HandleOther(conn)
+		}
+
+	}
+}
