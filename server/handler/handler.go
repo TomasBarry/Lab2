2eeb5441cbd690fed162549f650ed0e06583ae55
Tomas Barry <tomas.barry@hotmail.com>
2016-11-09 10:54:53 +0000

    Fixed compiler warning

--- server/handler/handler.go
+++ server/handler/handler.go
@@ -15,7 +15,7 @@ func HandleHelo(message string, conn net.Conn) {
 	// conn.LocalAddr() returns "10.62.0.117:8000" for example
 	// serverInfo[0] = 10.62.0.117
 	// serverInfo[1] = 8000
-	serverInfo := strings.Split(conn.LocalAddr(), ":")
+	serverInfo := strings.Split(conn.LocalAddr().String(), ":")
 	conn.Write([]byte(fmt.Sprintf("%sIP:%s\nPort:%s\nStudentID:%s\n", message, serverInfo[0], serverInfo[1], "13321218")))
 }
 
